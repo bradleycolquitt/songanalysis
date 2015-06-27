07f45f3de22ca877ace835ddd86ff0d0e331bfe5
lyre <bradley.colquitt@gmail.com>
2015-06-27 14:33:19 -0700

    added interpeak distance filtering to cleanSongDir.r

--- cleanSongDir.r
+++ cleanSongDir.r
@@ -1,20 +1,23 @@
 #! /usr/bin/env r
 
 suppressMessages(source("/media/data2/rstudio/birds/song/song_util.R"))
-
+suppressMessages(library(parallel))
 suppressMessages(library(docopt))      
 
-doc <- "Usage: cleanSongDir.r [-d directory]
+doc <- "Usage: cleanSongDir.r [-d directory] [-t thresh] [-c cores]
 
--d --dir DIRECTORY  directory to be processed
--t --thresh THRESH Weiner entropy threshold, 0 < t < 1
+-d --dir DIRECTORY  directory to be processed, default = '.'
+-t --thresh THRESH Weiner entropy threshold, 0 < t < 1, default = 0.4
+-c --cores CORES number of cores to use, default = 4
 -h --help           show this help text"
 
 opt <- docopt(doc)
 if(opt$dir == "NA")
   opt$dir = "."
-if(opt$thresh == "NA")
+if(length(opt$thresh) == 0)
   opt$thresh = .4
+if(length(opt$cores) == 0)
+  opt$cores = 4
 
 files = list.files(opt$dir, pattern=".wav", full.names = T)
 
@@ -24,25 +27,21 @@ sized_dir = paste(opt$dir, "sized", sep="/")
 print(paste("Moving files greater than ", sized_thresh / 1000, " Mbytes to sized", sep=""))
 sizes = sapply(files, function(x) file.info(x)$size)
 dir.create(sized_dir, showWarnings = F)
-sapply(files[sizes>sized_thresh], function(x) file.copy(x, paste(sized_dir, x, sep="/")))
+sapply(files[sizes>sized_thresh], function(x) file.copy(x, paste(sized_dir, basename(x), sep="/"), overwrite = T))
 
 #### Filter by spectral info ####
 print("Loading WAVs...")
 sized_files = list.files(sized_dir, pattern=".wav", full.names = T)
-wav = lapply(sized_files, readWave)
-print("Calcuating PSDs...")
-psd = lapply(wav, function(x) spec(filtersong(x), wl=256, PSD=T, plot=F))
+wav = mclapply(sized_files, readWave, mc.cores=opt$cores)
+
 print("Finding songs...")
-res = sapply(psd, function(x) {
-  songfinder_psd(x, band=c(5,7), sum_thresh=0, var_thresh=0, wein_thresh=.4)
-})
+res = unlist(mclapply(wav, function(w) {
+  songfinder(w, band = c(5,7), wein_thresh = opt$thresh)
+}, mc.cores=opt$cores))
 res = data.frame(name=sized_files, song=res) 
+
 songs_dir = paste(opt$dir, "songs", sep="/")
 dir.create(songs_dir, showWarnings = F)
-
-#file.copy(as.character(res$name[res[,2]]), songs_dir)
 for (ind in which(res[,2])) {
-  print(as.character(res[ind,1]))
   file.rename(as.character(res[ind,1]), paste(songs_dir, basename(as.character(res[ind,1])), sep="/"))
-  #file.rename(as.character(res[ind,1]), songs_dir)
 }
