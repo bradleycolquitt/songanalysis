8dd3d76f48c48999b9de25ac797a4503d949d372
lyre <bradley.colquitt@gmail.com>
2015-06-30 10:48:30 -0700

    clean up and extend database write of computeSongStats.r

--- computeSongStats.r
+++ computeSongStats.r
@@ -1,65 +1,124 @@
 #! /usr/bin/env r
 
+#
+# 
+#
+#
+#
+
 suppressMessages(source("/media/data2/rstudio/birds/song/song_util.R"))
 suppressMessages(library(parallel))
-suppressMessages(library(docopt))      
+suppressMessages(library(docopt))  
+suppressMessages(library(rPython))
+suppressMessages(library(stringr))
 
-doc <- "Usage: computeSongStats.r [-d directory] [-e euthanasia] [-c cores]
+doc <- "Usage: computeSongStats.r [-d directory] [-db db] [-i bird_info] [-c cores]
 
 -d --dir DIRECTORY  directory to be processed, default = '.'
--e --euth TIME time of euthanasia
--t --thresh THRESH Weiner entropy threshold, 0 < t < 1, default = 0.4
+-e --bird_info INFO csv file containing information about given bird
+-db DATABASE database to write to
 -c --cores CORES number of cores to use, default = 4
 -h --help           show this help text"
 
 opt <- docopt(doc)
 if(opt$dir == "NA")
   opt$dir = "."
-if(length(opt$thresh) == 0)
-  opt$thresh = .4
 if(length(opt$cores) == 0)
   opt$cores = 4
 
+setwd("/media/data2/song/bl63bl44/songs")
+opt = list(dir=".", thresh=.4, cores=10, bird_info="/media/data2/song/bl63bl44/bird_info.csv")
 files = list.files(opt$dir, pattern=".wav", full.names = T)
+file_info = file.info(files)
+#file_info$rank = 1:nrow(file_info)
 
-#### Bird ID ####
-dirs = str_split(files, "/")
-bird_id = dirs[length(dirs)-1] # expects bird_id/songs directory structure
-#### Time of euthanasia ####
-# given, combine with final date
-
-#### Song number ####
-song_number = seq(1, length(files))
-#### Song timing ####
-#### Motif rate ####
-#### Motifs per song ####
-#### Syllables per song ####
-#### Gaps ####
-#### Syllable durations ####
-#### Weiner entropies ####
-
-
-#### Filter by size ####
-sized_thresh = 400000
-sized_dir = paste(opt$dir, "sized", sep="/")
-print(paste("Moving files greater than ", sized_thresh / 1000, " Mbytes to sized", sep=""))
-sizes = sapply(files, function(x) file.info(x)$size)
-dir.create(sized_dir, showWarnings = F)
-sapply(files[sizes>sized_thresh], function(x) file.copy(x, paste(sized_dir, basename(x), sep="/"), overwrite = T))
-
-#### Filter by spectral info ####
-print("Loading WAVs...")
-sized_files = list.files(sized_dir, pattern=".wav", full.names = T)
-wav = mclapply(sized_files, readWave, mc.cores=opt$cores)
-
-print("Finding songs...")
-res = unlist(mclapply(wav, function(w) {
-  songfinder(w, band = c(5,7), wein_thresh = opt$thresh)
-}, mc.cores=opt$cores))
-res = data.frame(name=sized_files, song=res) 
-
-songs_dir = paste(opt$dir, "songs", sep="/")
-dir.create(songs_dir, showWarnings = F)
-for (ind in which(res[,2])) {
-  file.rename(as.character(res[ind,1]), paste(songs_dir, basename(as.character(res[ind,1])), sep="/"))
+results = lapply(1:length(files), function(ind) {
+  result = list() 
+  wav = readWave(files[[ind]])
+  
+  ### Song-level analysis ###
+  print(files[[ind]])
+  result$songs = findpeaks(wav, min_duration=1000, max_gap=300, thresh=0.05)
+  result$songs$song_number = ind
+  
+  ### Motif level analysis ###
+  result$motifs = findpeaks(wav, min_duration=200, max_gap=50, thresh=0.05)
+  if (is.null(result$motifs)) return(result)
+  result$motifs$song_number = ind
+  result$motifs$motif_number = 1:nrow(result$motifs)
+  colnames(result$motifs)[1:2] = c("motif_start", "motif_end")
+  
+  #### Syllable level analysis####
+  result$syllables = findpeaks(wav, min_duration=20, max_gap=10, thresh=0.05)
+  if (is.null(result$syllables)) return(result)
+  
+  result$syllables$midpoint = rowSums(result$syllables[,1:2]) / 2
+  motif_breaks = c(0, interpeak_midpoint(result$motifs), length(wav@left) / wav@samp.rate)
+  result$syllables$motif_number = cut(result$syllables$midpoint, 
+                        breaks=motif_breaks, 
+                        include.lowest=F, 
+                        labels=F)
+  result$syllables$song_number = ind
+  result$syllables$durations = result$syllables[,2] - result$syllables[,1]
+  
+  #### Weiner entropies ####
+  result$syllables$ent = apply(result$syllables, 1, function(syl) {
+    weiner_entropy(wav, band=c(5,10), subregion=syl)
+  })
+  return(result)
 }
+)
+#,mc.cores=opt$cores)
+names(results) = files
+
+#### Write to DB ####
+conn = dbConnect(RMySQL::MySQL(), group="songdb")
+bird_df = read.csv(opt$bird_info, header=T)
+dbWriteTable(conn, "birds", bird_df, append=TRUE, row.names=FALSE)
+
+#### Songs ####
+songs_timings = lapply(results, function(result) {
+                      result[['songs']]
+                    })
+songs_df = do.call("rbind", songs_timings)
+file_info = cbind(file_info, songs_df)
+file_info$bird_id = bird_df$bird_id
+
+song_formatted = file_info[,c("bird_id", "mtime", "ins", "outs", "song_number")]
+colnames(song_formatted) = c("bird_id", "song_datetime", "song_start", "song_end", "song_number")
+dbWriteTable(conn, "songs", song_formatted, append=TRUE, row.names=F)
+
+#### Motifs ####
+motifs = lapply(results, function(result) {
+  song_ids = apply(result[['motifs']], 1, function(motif) {
+    res = dbSendQuery(conn, paste("SELECT song_id FROM songs WHERE song_number = ", motif['song_number'], 
+                            " AND bird_id = ", bird_df$bird_id, sep=""))
+    df = dbFetch(res)
+    dbClearResult(res)
+    return(df)
+  })
+  result[['motifs']] = data.frame(result[['motifs']], song_id=unlist(song_ids))
+  result[['motifs']]
+})
+motifs_df = do.call("rbind", motifs)
+dbWriteTable(conn, 'motifs', motifs_df[,c("song_id", "motif_number", "motif_start", "motif_end")], append=T, row.names=F)
+
+#### Syllables ####
+syllables = lapply(results, function(result) {
+   motif_ids = apply(result[['syllables']], 1, function(syllables) {
+    res = dbSendQuery(conn, paste("SELECT motif_id FROM motifs JOIN songs WHERE motifs.motif_number = ", 
+                                  syllables['motif_number'], 
+                                  " AND songs.song_number = ",
+                                  syllables['song_number'],
+                                  " AND bird_id = ", bird_df$bird_id,
+                                  sep=""))
+    df = dbFetch(res)
+    dbClearResult(res)
+    return(df)
+  })
+  result[['syllables']] = data.frame(result[['syllables']], motif_id=unlist(motif_ids))
+  result[['syllables']]
+})
+syllables_df = do.call("rbind", syllables)
+dbWriteTable(conn, 'syllables', motifs_df[,c("song_id", "motif_number", "motif_start", "motif_end")], append=T, row.names=F)
+
