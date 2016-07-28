2864101a86dddff767f64b34c8f3e5a8dd0c8ec8
Brainard Lab <osprey@osprey.cin.ucsf.edu>
2016-07-28 11:37:57 -0700

    deleted test lines

--- findSongs.r
+++ findSongs.r
@@ -21,7 +21,7 @@ options(warn=-1)
 suppressMessages(source("~/src/songanalysis/song_util.R"))
 suppressMessages(source("~/src/songanalysis/threshold.r"))
 
-opt = list(dir="/mnt/bengal_home/song/bk47/2016-07-25", cores=10)
+#opt = list(dir="/mnt/bengal_home/song/bk47/2016-07-25", cores=10)
 #opt = list(dir="/mnt/bengal_home/song/gr44gr48", no_filter_by_size=1, cores=10)
 
 if (length(opt$date) == 0 & length(opt$time) > 0)
@@ -77,7 +77,7 @@ if (!opt$no_filter_by_size) {
 }
 #### Find songs ####
 print("Finding songs...")
-sized_files = sized_files[1:10]
+#sized_files = sized_files[1:10]
 res = unlist(mclapply(sized_files, function(file) {
   w = readWave(file)
   songfinder2(w, min_duration = 15, max_gap = 10, max_duration=200,
